9bb68ce08f765936308634bdf6a71ff71848a865
NikitaBaulin <134143545+NikitaBaulin@users.noreply.github.com>
2023-05-21 14:55:47 +0300

    Add files via upload

---
+++ finale_app.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtWigets
+
+
+
+class FirstScreen(QtWigets):
+    def ___init___(self):
+        self.set_appears()
+        self.initUI()
+        self.connect()
+        self.show()
+    def set_appear(self):
+     
